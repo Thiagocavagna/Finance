8afdb3a41ef6143deb0618eda52200fa6abaf9b6
Abner da Silva Santos <93294372+AbnerSantos25@users.noreply.github.com>
2024-07-14 12:46:26 -0300

    transaction

---
+++ Finance/Data/Repositories/ITransactionRepository.cs
@@ -0,0 +1,13 @@
+﻿using Finance_Project.Model.Entities;
+
+namespace Finance.Data.Repositories
+{
+    public interface ITransactionRepository
+    {
+        public void Add(Transaction transaction);
+        public void Delete(Transaction transaction);
+        public void Update(Transaction transaction);
+        public IEnumerable<Transaction> getAll();
+        public void Save();
+    }
+}
